95143971787d753de6695decf9688a422c7dcec9
joynchou <602503587@qq.com>
2017-07-18 10:30:56 +0800

    更新

--- SOFTWARE/TASK/setup.c
+++ SOFTWARE/TASK/setup.c
@@ -51,69 +51,6 @@ void    UART1_init(unsigned long BandRate)
 	USART_Configuration(USART1, &COMx_InitStructure);
 	// PrintString1("STC15w4k58s4's UART1 is open \r\n");   //串口测试
 }
-void test(void)//器件测试
-{
-		//stepMotor_Init (STEP_MOTOR_1,1.8f,4,6000);
-	//setStepMotorWithRound(STEP_MOTOR_1,30,100);   
-//	Init_Str_Motor(SERVO_1,0.5f,2.5f,180,300);
-//	//Init_Str_Motor(SERVO_2,0.5f,2.5f,180,300);
-//		//open_STR(SERVO_1);
-//   //	set_STR_angle(SERVO_1,1);
-//	    
-
-//   for(;i<180;i++)
-//	{
-//	set_STR_angle(SERVO_1,i);
-//  open_StepMotor(STEP_MOTOR_1);
-//		delay_ms(200);
-//		delay_ms(200);
-//		delay_ms(200);
-//	}
-	//open_STR(SERVO_2);
-  	int i = 0;
-	GPIO_InitTypeDef    GPIO_InitStructure;     //结构定义
-	PWM_InitTypeDef  PWM_InitStructure;
-
-
-	GPIO_InitStructure.Mode = GPIO_PullUp;       //指定IO的输入或输出方式,GPIO_PullUp,GPIO_HighZ,GPIO_OUT_OD,GPIO_OUT_PP
-
-	GPIO_InitStructure.Pin = GPIO_Pin_7;    //指定要初始化的IO, GPIO_Pin_0 ~ GPIO_Pin_7, 或操作
-	GPIO_Inilize(GPIO_P3, &GPIO_InitStructure);  //初始化
-	P37 = 1;
-	PWM_UNLOCK;
-	PWM_InitStructure.PWM_GOTO_ADC = DISABLE;
-	PWM_InitStructure.PWM_V_INIT = PWM_LOW;
-	PWM_InitStructure.PWM_0ISR_EN = DISABLE;
-	PWM_InitStructure.PWM_OUT_EN = ENABLE;
-	PWM_InitStructure.PWM_UNUSUAL_EN = DISABLE;
-	PWM_InitStructure.PWM_UNUSUAL_OUT = DISABLE;
-	PWM_InitStructure.PWM_UNUSUAL_ISR_EN = DISABLE;
-	PWM_InitStructure.PWM_UNUSUAL_CMP0_EN = DISABLE;
-	PWM_InitStructure.PWM_UNUSUAL_P24_EN = DISABLE;
-	PWM_InitStructure.PWM_CLOCK = PWM_Clock_NT;
-	PWM_InitStructure.PWM_CLOCK_DIV = 15;
-	PWM_InitStructure.PWM_SELECTx_IO = PWM_SELECT_N;
-	PWM_InitStructure.PWM_ISRx_EN = DISABLE;
-	PWM_InitStructure.PWM_T1x_EN = DISABLE;
-	PWM_InitStructure.PWM_T2x_EN = DISABLE;
-	PWM_InitStructure.PWM_EN = DISABLE;
-	PWM_Inilize(PWM_2, &PWM_InitStructure);
-	PWM_LOCK;
-	setPWM_DIV(PWM_2, 16);//16分频
-	set_PWM_period(PWM_2, 50);
-	set_PWM_duty(PWM_2, 0.1f);
-	open_PWM_N(PWM_2);
-
-	for (i = 100; i < 1000; i++)
-	{
-		set_PWM_duty(PWM_2, i / 1000.0f);
-		delay_ms(200);
-		delay_ms(200);
-		delay_ms(200);
-
-	}
-
-}
 void setup(void)
 {
 	UART1_init(115200L);
@@ -123,7 +60,6 @@ void setup(void)
 	ADC_config(ADC_P10, ADC_540T);
 	LCD1602_Init();
 	EA = 1;
-  test();
 
 }
 
