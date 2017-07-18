43a73c56355ff22b97b7e998dc7669a7a337748e
VrigoXia <1350621044@qq.com>
2017-07-18 11:40:09 +0800

    新建框架
    新建了任务框架，并注释

--- SOFTWARE/ABS_TASK/absTask.c
+++ SOFTWARE/ABS_TASK/absTask.c
@@ -9,99 +9,36 @@
 #include "../HARDWARE/DEVICES/SENSOR/ANGLE/ANGLE.h"
 #include "../COMMON_SOFTWARE/DATA_SCOPE/DataScope_DP.h"
 #include "../COMMON_SOFTWARE/STORAGE/STORAGE.h"
+#include "../../HARDWARE/BSP/STC15_PWM.H"
+#include "../SOFTWARE/ALGORITHM/PID/PID.H"
 
-void taskStart() _task_ 0
-{ 
-	setup();
-	blink(4);
-		os_create_task (1);
-	//	os_create_task (2);
-	os_create_task(3);
-	os_create_task(5);
-	//	os_create_task (6);
-	os_create_task(7);
-	os_create_task(8);
-	os_create_task(9);
-	os_delete_task(0);
-}
-void task1(void) _task_ 1
+#define INIT 0//初始化
+#define BUTTON 1//按键检测
+#define ANGLE 2//角度控制
+#define MODE_1 3//题目一
+#define MODE_2 4//题目二
+
+void Task_Init() _task_ INIT
 {
-  u8 i=0;
-	for (;;)
-	{
-
-		os_wait(K_IVL, 40, 0);
-
-	}
-}
-void task2(void) _task_ 2
-{
-	static unsigned int count = 0;
-	unsigned int test[20];
-
-
-
-
-}
-void task3(void) _task_ 3
-{
-	unsigned int string[50];
-	static unsigned int count = 0;
-	u8 i=10;
-	for (;;)
-	{   i++;
-
-     os_wait(K_IVL, 200, 0);
-
-	}
-
-}
-void task5(void) _task_ 5
-{
-	unsigned long count = 0;
-	for (;;)
-	{
-
-		buttonScan();
-		os_wait(K_IVL, 2, 0);
-	}
-}
-
-void task7(void) _task_ 7
-{
-	for (;;)
-	{
-		if (getButtonState(BUTTON1_GPIO_PIN))
-		{
-			open_Board_LED();
-		}
 	
-		os_wait(K_IVL, 10, 0);
-	}
 }
-void task8(void) _task_ 8
+
+void Task_Button() _task_ BUTTON
 {
-	for (;;)
-	{
-		if (getAngle(1) > 50)
-		{
-			open_Board_LED();
-		}
-		else
-		{
-			close_Board_LED();
-		}
-		os_wait(K_IVL, 1, 0);
-	}
+	
 }
-void task9(void) _task_ 9
+
+void Task_Angle() _task_ ANGLE
 {
-	for (;;)
-	{
-		sendScopeData(getAngle(1), 1);
-		os_wait(K_IVL, 40, 0);
-
-	}
-
+	
 }
 
+void Task_Mode_1() _task_ MODE_1
+{
+	
+}
+
+void Task_Mode_2() _task_ MODE_2
+{
+	
+}
